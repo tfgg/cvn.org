a0c4953d981f98ba2beec08f226df4bcb451c789
timothy.green <timothy.green@220e2412-9961-11de-a05d-3bd73e269465>
2009-09-10 18:27:08 +0000

    test strings, again
    git-svn-id: https://constituencyvolunteernetwork.googlecode.com/svn/trunk@22 220e2412-9961-11de-a05d-3bd73e269465

--- invite/tests.py
+++ invite/tests.py
@@ -7,12 +7,12 @@ from signup.models import CustomUser
 
 import strings
 
-M8R_PRE = "cvn-testing-"
-M8R_ADDR = {'frank': M8R_PRE+"frank@mailinator.com", 
-            'barry': M8R_PRE+"barry@mailinator.com", 
-            'carl': M8R_PRE+"carl@mailinator.com", }
+m8r_pre = "cvn-testing-"
+m8r_addr = {'frank': m8r_pre+"frank@mailinator.com", 
+            'barry': m8r_pre+"barry@mailinator.com", 
+            'carl': m8r_pre+"carl@mailinator.com", }
 
-USERS = [{'email': M8R_ADDR['frank'],
+USERS = [{'email': m8r_addr['frank'],
           'postcode': 'G206BT',
           'can_cc': True,
           'first_name': 'Frank',
@@ -34,37 +34,37 @@ class TestInvite(TestCase):
     
     def test_send_registered_invite(self):
         """ Send an invite to an already registered user """
-        invite_form = {'honeypot': '', 'email': M8R_ADDR['frank'], 'message': 'flump'}
+        invite_form = {'honeypot': '', 'email': m8r_addr['frank'], 'message': 'flump'}
         response = self.client.post("/invite/", invite_form, follow=True) # Send the invite        
-        self.assertTrue(strings.INVITE_ERROR_REGISTERED % M8R_ADDR['frank'] in response.content)
+        self.assertTrue(strings.INVITE_ERROR_REGISTERED % m8r_addr['frank'] in response.content)
     
     def test_send_invite(self):
         """ Send a valid invite to an email address, then send an invite again to the same email address """
-        invite_form = {'honeypot': '', 'email': M8R_ADDR['barry'], 'message': 'flump'}
+        invite_form = {'honeypot': '', 'email': m8r_addr['barry'], 'message': 'flump'}
         response = self.client.post("/invite/", invite_form, follow=True)
         self.assertTrue(strings.INVITE_NOTICE_SUCCESS in response.content)
         self.assertEquals(len(mail.outbox),1)
-        self.assertEquals(mail.outbox[0].subject, strings.INVITE_SUBJECT % M8R_ADDR['frank'])
-        self.assertEquals(Invitation.objects.filter(email=M8R_ADDR['barry']).count(), 1)
+        self.assertEquals(mail.outbox[0].subject, strings.INVITE_SUBJECT % m8r_addr['frank'])
+        self.assertEquals(Invitation.objects.filter(email=m8r_addr['barry']).count(), 1)
         
-        invite_form = {'honeypot': '', 'email': M8R_ADDR['barry'], 'message': 'flump'}
+        invite_form = {'honeypot': '', 'email': m8r_addr['barry'], 'message': 'flump'}
         response = self.client.post("/invite/", invite_form, follow=True)
-        self.assertTrue(strings.INVITE_ERROR_INVITED % M8R_ADDR['barry'] in response.content)
+        self.assertTrue(strings.INVITE_ERROR_INVITED % m8r_addr['barry'] in response.content)
         
     def test_send_invite_multi(self):
         """ Send a multi-address email """
-        invite_form = {'honeypot': '', 'email': 'Barry <%s>, Carl <%s>' % (M8R_ADDR['barry'], M8R_ADDR['carl']), 'message': 'flump'}
+        invite_form = {'honeypot': '', 'email': 'Barry <%s>, Carl <%s>' % (m8r_addr['barry'], m8r_addr['carl']), 'message': 'flump'}
         response = self.client.post("/invite/", invite_form, follow=True)
         self.assertTrue(strings.INVITE_NOTICE_SUCCESS in response.content)
         self.assertEquals(len(mail.outbox),2)
-        self.assertEquals(mail.outbox[0].subject, strings.INVITE_SUBJECT % M8R_ADDR['frank'])
+        self.assertEquals(mail.outbox[0].subject, strings.INVITE_SUBJECT % m8r_addr['frank'])
         self.assertEquals(Invitation.objects.all().count(), 2)
-        self.assertEquals(Invitation.objects.filter(email=M8R_ADDR['barry']).count(), 1)
-        self.assertEquals(Invitation.objects.filter(email=M8R_ADDR['carl']).count(), 1)
+        self.assertEquals(Invitation.objects.filter(email=m8r_addr['barry']).count(), 1)
+        self.assertEquals(Invitation.objects.filter(email=m8r_addr['carl']).count(), 1)
     
     def test_honeypot(self):
         """ Test the bot honeypot on the invitation form """
-        invite_form = {'honeypot': 'BLAH BLAH IM A BOT', 'email': M8R_ADDR['barry'], 'message': 'flump'}
+        invite_form = {'honeypot': 'BLAH BLAH IM A BOT', 'email': m8r_addr['barry'], 'message': 'flump'}
         response = self.client.post("/invite/", invite_form, follow=True)
         self.assertTrue(strings.INVITE_ERROR_HONEYPOT in response.content)
         
